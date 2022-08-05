e69ce83834f3ac56c3bca56651bcc8636ae888b5
Arjix <53124886+ArjixWasTaken@users.noreply.github.com>
2022-08-06 00:08:29 +0300

    Make the status bar's color be tranparent

--- app/src/main/java/com/arjixwastaken/nyaadroid/ui/theme/Theme.kt
+++ app/src/main/java/com/arjixwastaken/nyaadroid/ui/theme/Theme.kt
@@ -1,10 +1,17 @@
 package com.arjixwastaken.nyaadroid.ui.theme
 
+import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.compositeOver
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorPalette = darkColors(
         primary = Purple200,
@@ -33,6 +40,17 @@ fun NyaaDroidTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composa
         DarkColorPalette
     } else {
         LightColorPalette
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val activity  = view.context as Activity
+            activity.window.navigationBarColor = colors.primary.copy(alpha = 0.08f).compositeOver(colors.surface.copy()).toArgb()
+            activity.window.statusBarColor = colors.background.toArgb()
+            WindowCompat.getInsetsController(activity.window, view).isAppearanceLightStatusBars = !darkTheme
+            WindowCompat.getInsetsController(activity.window, view).isAppearanceLightNavigationBars = !darkTheme
+        }
     }
 
     MaterialTheme(
